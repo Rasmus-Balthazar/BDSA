efb60e48241123f7da51cd9554eee30fb3dfdfc6
Rasmus-Balthazar <74965526+Rasmus-Balthazar@users.noreply.github.com>
2021-09-05 14:08:22 +0200

    Tested and implemented IsLeapYear (#2)
    * add initial project structure
    
    * Tested and implemented IsLeapYear

---
+++ exercise00.Tests/UnitTest1.cs
@@ -0,0 +1,51 @@
+using System;
+using Xunit;
+using System.IO;
+
+namespace exercise00.Tests
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(4)]
+        [InlineData(16)]
+        [InlineData(20)]
+        public void Year_DivisableBy4_ReturnTrue(int value)
+        {
+            bool expected = true;
+            var actual = Program.IsLeapYear(value);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(13)]
+        [InlineData(17)]
+        public void Year_NotDivisableBy4_ReturnFalse(int value)
+        {
+            bool expected = false;
+            var actual = Program.IsLeapYear(value);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(400)]
+        [InlineData(1600)]
+        [InlineData(2000)]
+        public void Year_DivisableBy400_ReturnFalse(int value)
+        {
+            bool expected = true;
+            var actual = Program.IsLeapYear(value);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(1300)]
+        [InlineData(600)]
+        [InlineData(300)]
+        public void Year_DivisableBy100_Not400_ReturnFalse(int value){
+            bool expected = false;
+            var actual = Program.IsLeapYear(value);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ exercise00/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exercise00
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static bool IsLeapYear(int year)
+        {
+            if(year%400 == 0)
+            {   
+                return true;
+            } else if(year%100 == 0)
+            {
+                return false;
+            } else if(year%4 == 0)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+
+    }
+}
